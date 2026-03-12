dc5db127bff2712db6012f0b5d9730fd62a50ceb
PurplePlatform2 <ovegies9ja@gmail.com>
2026-03-12 08:18:19 +0100

    Use environment variable for server port

--- main.py
+++ main.py
@@ -187,8 +187,10 @@ if __name__ == "__main__":
 
     import uvicorn
 
+    port = int(os.environ.get("PORT",8000))
+
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=8000
+        port=port
     )
